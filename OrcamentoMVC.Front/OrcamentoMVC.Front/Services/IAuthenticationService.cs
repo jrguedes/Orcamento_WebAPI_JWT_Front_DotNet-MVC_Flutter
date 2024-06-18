ffa15ad3da8bb94b2c4632d571358877e6b59853
Júnior Guedes <jrguedes.ja@gmail.com>
2024-06-18 11:33:01 -0300

    Authentication

---
+++ OrcamentoMVC.Front/OrcamentoMVC.Front/Services/IAuthenticationService.cs
@@ -0,0 +1,8 @@
+﻿using OrcamentoMVC.Front.Models;
+
+namespace OrcamentoMVC.Front.Services;
+
+public interface IAuthenticationService
+{
+    Task<TokenViewModel> AuthenticateUser(UserViewModel userVM);
+}
