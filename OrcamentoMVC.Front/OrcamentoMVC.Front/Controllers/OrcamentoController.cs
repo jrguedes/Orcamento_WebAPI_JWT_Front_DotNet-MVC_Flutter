76c3c110b23b179f97ca48c033c51fcb0ce2d966
Júnior Guedes <jrguedes.ja@gmail.com>
2024-07-04 15:56:08 -0300

    Ajustando template Bootstrap Topic Listing - https://themewagon.github.io/TopicListing/index.html

---
+++ OrcamentoMVC.Front/OrcamentoMVC.Front/Controllers/OrcamentoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrcamentoMVC.Front;
+
+public class OrcamentoController : Controller
+{
+    public IActionResult Index()
+    {        
+        return View();
+    }
+}
