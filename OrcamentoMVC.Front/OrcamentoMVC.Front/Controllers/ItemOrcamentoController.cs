d2b3bc002ea91e844cc8ead32f7042ccc6ccfe43
Júnior Guedes <jrguedes.ja@gmail.com>
2024-07-15 11:29:00 -0300

    Separando Orcamento de itens

---
+++ OrcamentoMVC.Front/OrcamentoMVC.Front/Controllers/ItemOrcamentoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrcamentoMVC.Front;
+[Route("Orcamento/[controller]")]
+public class ItemOrcamentoController : Controller
+{
+    public IActionResult Index()
+    {        
+        return View();
+    }
+}
