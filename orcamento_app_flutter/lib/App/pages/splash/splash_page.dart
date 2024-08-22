28993399c342a00613fffead8e2b53a190f5b951
Júnior Guedes <jrguedes.ja@gmail.com>
2024-08-22 15:39:24 -0300

    Tela de splash

--- orcamento_app_flutter/lib/App/pages/splash/splash_page.dart
+++ orcamento_app_flutter/lib/App/pages/splash/splash_page.dart
@@ -7,30 +7,33 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        /*
-      child: Material(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const FaIcon(
-              FontAwesomeIcons.circleDollarToSlot,
-              color: Colors.amber,
-              size: 350,
-            ),
-            Text(
-              title,
-              style: const TextStyle(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const FaIcon(
+                FontAwesomeIcons.circleDollarToSlot,
                 color: Colors.amber,
-                fontSize: 80,
-                fontWeight: FontWeight.w700,
+                size: 350,
               ),
-            ),
-          ],
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.amber,
+                  fontSize: 80,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),*/
-        );
+      ),
+    );
   }
 }
